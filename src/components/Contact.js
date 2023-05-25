b5da1e3a8f96c239c4760dbf4ad78024c8481ed7
youjin kim <gracecancho@gmail.com>
2023-05-25 17:55:36 +0900

    email 수정 완료

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
+    const form = useRef();
     const formInitialDetails = {
         firstName: '',
         lastName: '',
@@ -23,24 +25,22 @@ export const Contact = () => {
         })
     }
 
-    const handleSubmit = async (e) => {
+    const sendEmail = (e) => {
         e.preventDefault();
         setButtonText("Sending...");
-        let response = await fetch("http://localhost:3000/contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: JSON.stringify(formDetails),
-        });
-        setButtonText("Send");
-        let result = await response.json();
-        setFormDetails(formInitialDetails);
-        if (result.code === 200) {
-            setStatus({ succes: true, message: 'Message sent successfully' });
-        } else {
-            setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
-        }
+        const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+        const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+        const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY;
+
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+            .then((result) => {
+                alert('전송되었습니다!');
+                setButtonText("Send");
+                setFormDetails({ ...formInitialDetails });
+            }, (error) => {
+                console.log(error.text);
+            });
+
     };
 
     return (
@@ -59,22 +59,25 @@ export const Contact = () => {
                             {({ isVisible }) =>
                                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                                     <h2>Get In Touch</h2>
-                                    <form onSubmit={handleSubmit}>
+                                    <form ref={form} onSubmit={sendEmail}>
                                         <Row>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                                                <input type="text" name='firstName' value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                                                <input type="text" name='lastName' value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                                             </Col>
-                                            <Col size={12} sm={6} className="px-1">
+                                            {/* <Col size={12} sm={6} className="px-1">
                                                 <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                            </Col> */}
+                                            <Col size={12} sm={6} className="px-1">
+                                                <input type="text" name='email' value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                                                <input type="tel" name='phone' value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
                                             </Col>
                                             <Col size={12} className="px-1">
-                                                <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                                                <textarea rows="6" name='message' value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                                                 <button type="submit"><span>{buttonText}</span></button>
                                             </Col>
                                             {
