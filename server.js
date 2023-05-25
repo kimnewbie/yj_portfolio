08891d73f73af66ef3f204daed3c5c86c21be09e
youjin kim <gracecancho@gmail.com>
2023-05-25 16:16:20 +0900

    수정할 예정

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(3000, () => console.log("Server Running"));
+
+const EMAIL_USER = process.env.REACT_APP_EMAIL_USER;
+const EMAIL_PASS = process.env.REACT_APP_EMAIL_PASS;
+
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    // host: "smtp.ethereal.email",
+    // port: 587,
+    // secure: false, // true for 465, false for other ports
+    auth: { user: EMAIL_USER, pass: EMAIL_PASS },
+});
+
+contactEmail.verify((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Ready to Send");
+    }
+});
+
+router.post("/contact", (req, res) => {
+    const name = req.body.firstName + req.body.lastName;
+    const email = req.body.email;
+    const message = req.body.message;
+    const phone = req.body.phone;
+    const mail = {
+        from: name,
+        to: EMAIL_USER,
+        subject: "Contact Form Submission - Portfolio",
+        html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+        if (error) {
+            res.json(error);
+        } else {
+            res.json({ code: 200, status: "Message Sent" });
+        }
+    });
+});
